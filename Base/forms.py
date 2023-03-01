81493bb4e56b5e7a936bf3f4b44a2c75e3b9bf8f
Saniyaj Mallik <111429200+Saniyaj21@users.noreply.github.com>
2023-03-01 22:11:00 +0530

    Add files via upload

--- Base/forms.py
+++ Base/forms.py
@@ -14,10 +14,8 @@ class RegisterUserForm(UserCreationForm):
         fields = ['username','password1','password2']        
 
         widgets ={
-
             'username':forms.TextInput(attrs={'id':'form-input1'}),
-            'password1':forms.TextInput(attrs={'id':'id_password1'  }),
-
-            'password2':forms.TextInput(attrs={'id':'id_password2' }),
+            'password1':forms.TextInput(attrs={'id':'id_password1'}),
+            'password2':forms.TextInput(attrs={'id':'id_password2'}),
         }
 

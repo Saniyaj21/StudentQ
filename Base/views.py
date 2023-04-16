5348de4d1efbf7b4affb094d7787d0f4c6e0fb04
Soumen Samanta <111648383+Soumen3@users.noreply.github.com>
2023-04-16 20:57:04 +0530

    Add files via upload

--- Base/views.py
+++ Base/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
@@ -8,19 +8,18 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .models import Profile, Student, Teacher, Owner, Review,Tutorial, Institute, Notice
-from django.db.models import Q
+from .models import Profile, Student, Teacher, Owner, Review, Tutorial, Institute, Notice
 
 from django.core.files.storage import FileSystemStorage
 
-# notes 
+# notes
 # pip install -r requirements.txt       to download all required packages
 
 
 def loginPage(request):
 
     context = {}
-    
+
     if request.user.is_authenticated:
         return redirect('home')
     if request.method == "POST":
@@ -42,9 +41,8 @@ def loginPage(request):
             return redirect('home')
         else:
             messages.error(request, 'Username or passward does not exists')
-   
-    return render(request, 'login.html', context)
 
+    return render(request, 'login.html', context)
 
 
 def logoutUser(request):
@@ -52,11 +50,10 @@ def logoutUser(request):
     return redirect('home')
 
 
-
 def registerUser(request):
-    
+
     form = RegisterUserForm()
-  
+
     if request.method == "POST":
         form = RegisterUserForm(request.POST)
         # form = UserCreationForm(request.POST)
@@ -67,72 +64,67 @@ def registerUser(request):
             login(request, user)
             profile = Profile.objects.create(user=user)
             return redirect('profile-details')
-                        
+
         else:
             messages.error(request, 'Something error happend,Try again!')
 
     return render(request, 'register.html', {'form': form})
 
 # @login_required(login_url='login')
+
+
 def home(request):
     context = {}
 
     # user counting
     userCount = User.objects.all().count()
- 
-    context['TotalUser'] = userCount
 
- 
-  
+    context['TotalUser'] = userCount
 
     if request.user.is_authenticated:
 
         dp = request.user.profile.dp
         context['dp'] = dp
 
-        if request.user.profile.role == 'student': 
-            student = Student.objects.get(student_userid  = request.user.id)
+        if request.user.profile.role == 'student':
+            student = Student.objects.get(student_userid=request.user.id)
             context["student"] = student
 
-        elif request.user.profile.role == 'teacher': 
-            teacher = Teacher.objects.get(teacher_userid  = request.user.id)
+        elif request.user.profile.role == 'teacher':
+            teacher = Teacher.objects.get(teacher_userid=request.user.id)
             context["teacher"] = teacher
 
-        if request.user.profile.role == 'owner': 
-            owner = Owner.objects.get(mess_userid  = request.user.id)
+        if request.user.profile.role == 'owner':
+            owner = Owner.objects.get(mess_userid=request.user.id)
             context["owner"] = owner
-    
+
     reviews = Review.objects.all()
     context["reviews"] = reviews
-    
-    
-    return render(request, 'home.html', context )
 
-
+    return render(request, 'home.html', context)
 
 
 def profileDetails(request):
     uId = request.user.id
-    user = User.objects.get(id = uId)
-    profile = Profile.objects.get(user = user)
+    user = User.objects.get(id=uId)
+    profile = Profile.objects.get(user=user)
     context = {}
 
     # print(profile.user)
 
     # profile data taking input
-    
+
     if request.method == "POST" and request.FILES['dp']:
         fullname = request.POST['full-name']
-        email =  request.POST['email'].lower()
+        email = request.POST['email'].lower()
         role = request.POST['role'].lower()
         phNo = request.POST['phNo']
         socialId = request.POST['socialId']
         desc = request.POST['desc']
         dp = request.FILES['dp']
-        
+
         fs = FileSystemStorage()
-        dp =fs.save(dp.name,dp)
-    
+        dp = fs.save(dp.name, dp)
 
         # print(fullname,email,role)
 
@@ -143,30 +135,28 @@ def profileDetails(request):
         profile.socialId = socialId
         profile.desc = desc
         profile.dp = dp
-       
 
         profile.save()
 
         if role == "teacher":
-            Teacher.objects.create(teacher_userid = uId)
+            Teacher.objects.create(teacher_userid=uId)
         elif role == "student":
-            Student.objects.create(student_userid = uId)
+            Student.objects.create(student_userid=uId)
             # that_student = Student.objects.get(student_userid = uId)
             # that_student.full_name = fullname
         elif role == "owner":
-            Owner.objects.create(mess_userid = uId)
-
+            Owner.objects.create(mess_userid=uId)
 
         return redirect("role-details")
-    
+
     return render(request, 'user_data.html', context)
 
 
 def roleDetails(request):
     uId = request.user.id
-    user = User.objects.get(id = uId)
-    profile = Profile.objects.get(user = user)
-    all_inst=Institute.objects.all()
+    user = User.objects.get(id=uId)
+    profile = Profile.objects.get(user=user)
+    all_inst = Institute.objects.all()
 
     page = 'student'
 
@@ -177,7 +167,7 @@ def roleDetails(request):
             institute = request.POST['institute']
             subject = request.POST['subject']
             interest = request.POST['interest']
-            inst_location=request.POST['institute_location']
+
             try:
                 institute_obj = Institute.objects.get(name=institute)
             except Institute.DoesNotExist:
@@ -185,11 +175,11 @@ def roleDetails(request):
 
             if institute_obj is None:
                 # Create new Institute object if it doesn't exist
-                institute_obj = Institute.objects.create(name=institute , institute_location=inst_location)
-                
+                institute_obj = Institute.objects.create(name=institute)
+
             courseDuration = request.POST['courseDuration']
-            
-            student = Student.objects.get(student_userid = uId)
+
+            student = Student.objects.get(student_userid=uId)
             fullName = request.user.profile.fullName
             print(fullName)
 
@@ -202,7 +192,6 @@ def roleDetails(request):
             student.save()
             return redirect('home')
 
-
     if profile.role == 'teacher':
         page = 'teacher'
 
@@ -210,16 +199,12 @@ def roleDetails(request):
             location = request.POST['location']
             dptOfTeaching = request.POST['dptOfTeaching']
             qualification = request.POST['qualification']
-            institute = request.POST['institute']
-            
-            
-            teacher = Teacher.objects.get(teacher_userid = uId)
+
+            teacher = Teacher.objects.get(teacher_userid=uId)
 
             teacher.location = location
             teacher.dptOfTeaching = dptOfTeaching
             teacher.qualification = qualification
-            teacher.institute =institute
-
 
             teacher.save()
             return redirect('home')
@@ -231,33 +216,30 @@ def roleDetails(request):
             rent = request.POST['rent']
             bedAvailable = request.POST['bedAvailable']
             address = request.POST['address']
-            institute = request.POST['institute']
-            
-            owner = Owner.objects.get(mess_userid = uId)
+
+            owner = Owner.objects.get(mess_userid=uId)
 
             owner.mess_name = mess_name
             owner.rent = rent
             owner.bedAvailable = bedAvailable
             owner.address = address
-            owner.institute=institute
 
             owner.save()
 
             return redirect('home')
-        
-    context ={'page':page}
-    
+
+    context = {'page': page}
+
     return render(request, 'role-details.html', context)
 
 
 # profile section
 @login_required(login_url='login')
-def profile(request,id):
+def profile(request, id):
     context = {}
 
-
     if request.user.is_authenticated:
-        profileUser = User.objects.get(id = id)
+        profileUser = User.objects.get(id=id)
         print(profileUser.username)
         context['profileUser'] = profileUser
 
@@ -265,23 +247,20 @@ def profile(request,id):
         context['dp'] = dp
         print(dp)
 
-        if profileUser.profile.role == 'student': 
-            student = Student.objects.get(student_userid  = id)
+        if profileUser.profile.role == 'student':
+            student = Student.objects.get(student_userid=id)
             context["student"] = student
 
-        elif profileUser.profile.role == 'teacher': 
-            teacher = Teacher.objects.get(teacher_userid  = id)
+        elif profileUser.profile.role == 'teacher':
+            teacher = Teacher.objects.get(teacher_userid=id)
             context["teacher"] = teacher
 
-        if profileUser.profile.role == 'owner': 
-            owner = Owner.objects.get(mess_userid  = id)
+        if profileUser.profile.role == 'owner':
+            owner = Owner.objects.get(mess_userid=id)
             context["owner"] = owner
-    
+
     return render(request, 'profile.html', context)
 
-
-
-        
 
 def postPage(request):
 
@@ -291,149 +270,113 @@ def postPage(request):
     name = user.username
 
     if request.method == "POST":
-            link = request.POST['link']
-            topic = request.POST['topic']
-            desc = request.POST['desc']
+        link = request.POST['link']
+        topic = request.POST['topic']
+        desc = request.POST['desc']
 
-            Tutorial.objects.create(
-                poster_user_id = poster_user_id,
-                name = name,
-                link = link,
-                topic = topic,
-                desc = desc
-            )
-            return redirect("studyMetirial")
-    return render(request,'post.html')
+        Tutorial.objects.create(
+            poster_user_id=poster_user_id,
+            name=name,
+            link=link,
+            topic=topic,
+            desc=desc
+        )
+        return redirect("studyMetirial")
+    return render(request, 'post.html')
+
 
 def Institutes(request):
     all_institutes = Institute.objects.all()
-    context ={}
+    context = {}
     context["all_institutes"] = all_institutes
     return render(request, "institute.html", context)
+
 
 @login_required(login_url='login')
 def Notices(request, id):
 
     context = {}
     context["id"] = id
-    institute = Institute.objects.get(id = id)
+    institute = Institute.objects.get(id=id)
     ins_notices = institute.notice_set.all().order_by('-id')
     context["ins_notices"] = ins_notices
-     
-    user=request.user
-    context['post_able']=False
 
-  
-    if user.profile.role=='student':
-        student=Student.objects.get(student_userid=user.id)
-        
-   
+    user = request.user
+    context['post_able'] = False
+
+    if user.profile.role == 'student':
+        student = Student.objects.get(student_userid=user.id)
+
         if institute.name == student.institute:
-            context['post_able']=True
+            context['post_able'] = True
 
     # all alumni of that institute
-    alumnis = Student.objects.filter(institute = institute)
+    alumnis = Student.objects.filter(institute=institute)
     # users = User.objects.all()
 
     context['alumnis'] = alumnis
     # context['users'] = users
-    
+
     # get all notice-poster profile
     noticePoster = Profile.objects.all()
-    context['noticePoster']=noticePoster
+    context['noticePoster'] = noticePoster
     # get when institute are select from user
-    context['instituteName']=institute
-    
-    # Allmess
-    # allMess=Owner.objects.filter(address__icontains = institute.institute_location).values()
-    allMess = Owner.objects.filter(
-    Q(institute__icontains=institute.name) | Q(institute=institute.name)
-).values()
-   
-    context['allMess']=allMess
-    
-    allTeachers = Teacher.objects.filter(institute = institute.name).values()
-   
-    context['allTeachers']=allTeachers
-   
-    print(allMess)
-    print(institute.institute_location)
-    print(allTeachers)
+    context['instituteName'] = institute
 
     return render(request, "notices.html", context)
 
 
+def PostNotice(request, id):
+    institute = Institute.objects.get(id=id)
 
-
-
-
-def PostNotice(request,id):
-    institute = Institute.objects.get(id = id)
-   
     if request.method == "POST" and request.FILES['notice']:
-        student=request.user.username
+        student = request.user.username
         notice = request.FILES['notice']
-        notice_caption=request.POST['notice_caption']
-        url=reverse('notices',args=[id])
-        Notice.objects.create(notice=notice , institute = institute , caption=notice_caption ,student=student)
+        notice_caption = request.POST['notice_caption']
+        url = reverse('notices', args=[id])
+        Notice.objects.create(notice=notice, institute=institute,
+                              caption=notice_caption, student=student)
         return redirect(url)
-     
+
     return render(request, "postNotice.html")
 
 
-
 @login_required(login_url='login')
-def delete_notice(request,id):
-    notice=Notice.objects.get(id=id)
+def delete_notice(request, id):
+    notice = Notice.objects.get(id=id)
     notice.delete()
     return redirect('institute')
     # url=reverse('notices',args=[id])
     # return render(request , "delete_msg.html")
-    
 
 
-           
+
 # study meririal
 def studyMetirial(request):
 
     user = request.user
-    context={}
+    context = {}
 
-    
+    if 'q' in request.GET:
+        # for searching
+        query = request.GET['q']
+        if len(query) > 100:
+            posts = Tutorial.objects.none()
+            query = query[0:10]+'.....'
+        else:
+            postsTopic = Tutorial.objects.filter(topic__icontains=query)
+            postsDescription = Tutorial.objects.filter(desc__icontains=query)
+            posts = postsTopic.union(postsDescription)
 
-    posts = Tutorial.objects.all()
-    context["posts"] = posts
-    return render(request,"study_metirial.html", context)
+        if posts.count() == 0:
+            messages.error(
+                request, 'No search result found, Please refine your query')
 
-        
-
-            
-# @login_required(login_url='login')          
-def search(request):
-    query=request.GET['query']
-    if len(query)>100:
-        posts=Tutorial.objects.none()
-        query=query[0:10]+'.....'
+        context = {'posts': posts, 'query': query}
+        return render(request, "study_metirial.html", context)
+    # end serching
     else:
-        postsTopic=Tutorial.objects.filter(topic__icontains=query)
-        postsDescription=Tutorial.objects.filter(desc__icontains=query)
-        posts=postsTopic.union(postsDescription)
-
-    if posts.count()==0:
-        messages.error(request, 'No search result found, Please refine your query')
-    
-    context={'posts':posts,'query':query}
-    return render(request, 'study_metirial.html', context)
-
-
-
-
-
-
-
-
-    
-
-
-
+        posts = Tutorial.objects.all()
+        context["posts"] = posts
+        return render(request, "study_metirial.html", context)
 
